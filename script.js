a08bba46155b1a9fcb797d2198d8f04138ca221c
Rohit singh <77694147+Rohit-singh19@users.noreply.github.com>
2021-06-02 15:33:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () =>{
+    setTimeout(()=>{
+        document.getElementById('pre-load').style.display = 'none';
+        document.getElementById('container').style.display = 'block';
+    },7000);
+});
